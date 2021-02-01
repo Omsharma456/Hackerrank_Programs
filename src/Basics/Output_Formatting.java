6213e67d94b0174d43937195814209d5eb39838f
Manish Sharma <omsharma571222@gmail.com>
2021-02-01 22:01:55 +0530

    Hackerrank Programs

--- src/Basics/Output_Formatting.java
+++ src/Basics/Output_Formatting.java
@@ -1,4 +1,15 @@
 package Basics;
+import java.util.*;
 
 public class Output_Formatting {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
 }
